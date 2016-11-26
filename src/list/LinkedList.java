93acaf98d3f06c1ae54cb189ac50124b9f7ce77f
kate <kateryna.maksimenko@gmail.com>
2016-11-26 21:34:04 +0200

    linked list

--- src/list/LinkedList.java
+++ src/list/LinkedList.java
@@ -3,5 +3,38 @@ package list;
 /**
  * Created by a1 on 10.08.16.
  */
-public class LinkedList {
+public class LinkedList<E> implements List<E> {
+
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    private static class Entry<E> {
+
+        E entry;
+        Entry<E> next;
+        Entry<E> prev;
+
+        public Entry(E entry, Entry<E> next, Entry<E> prev) {
+            this.entry = entry;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 }
